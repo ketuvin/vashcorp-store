b18bbe6edc36e910b0414084c425a9f0c11d308c
BentongX <fuenteskevin2015@gmail.com>
2022-11-10 17:10:58 +0800

    Create image viewer component

---
+++ components/ImageViewer.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const ImageViewer = (props) => {
+    return (
+        <div className="pl-0 pr-0">
+             <Image layout="fixed" width={456} height={370} src={props.src} className="" alt="Phone Logo" />
+        </div>
+    )
+};
+
+export default ImageViewer;
